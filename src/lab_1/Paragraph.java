0c3b30ea2cc5b13394385d1c52d4106f8912c6ba
Altynai98 <altynaiermekovna98@gmail.com>
2024-02-19 16:05:20 +0200

    Add Paragraph.sortParagraph method
    Add Paragraph.sortParagraph method

--- src/lab_1/Paragraph.java
+++ src/lab_1/Paragraph.java
@@ -1,10 +1,40 @@
 package lab_1;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections; // for the using sort method
 
 public class Paragraph {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    String[] paragraphList = new String[100];
+    
+    public Paragraph(String[] paragraphList) {
+        this.paragraphList = paragraphList;
+    }
+    
+    // Sorting words in alphabetical order
+    public ArrayList<String> sortParagraph() {
+        ArrayList<String> newParagraphList = new ArrayList<>();
+        for (int i = 0; i < this.paragraphList.length; i++) { 
+            newParagraphList.add(this.paragraphList[i]);
+        }
+        Collections.sort(newParagraphList); 
+        
+        return newParagraphList;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the paragraph:");
+        String paragraph = scanner.nextLine();
+        
+        String[] paragraphList = paragraph.split("\\s+");
+        Paragraph p = new Paragraph(paragraphList);
+        ArrayList<String> currentParagraph = p.sortParagraph();
+        
+        System.out.println("Sorted paragraph:");
+        for (String word : currentParagraph) {
+            System.out.print(word + " ");
+        }
+        scanner.close(); 
+    }
 }
